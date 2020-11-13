7e01c1d6a130f710f08dda98eb8af41c7ae714b3
tobesh01 <animashauntaofiq@yahoo.com>
2020-11-13 07:59:06 +0100

    New Update

--- main.js
+++ main.js
@@ -14,13 +14,7 @@ let displayMap = (lat, long) => {
         container._leaflet_id = null;
     }
 
-    let map = L.map('map', {
-        zoomControl: false,
-    });
-    L.control.zoom({
-        position: 'bottomright',
-    });
-    map.setView([lat, long], 13);
+    let map = L.map('map').setView([lat, long], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
