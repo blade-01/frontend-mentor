5e7b74dedbcba150cf6ca1b3bf23dc01d5881bc6
tobesh01 <animashauntaofiq@yahoo.com>
2020-11-13 08:08:43 +0100

    Zoom Level Changed

--- main.js
+++ main.js
@@ -14,7 +14,11 @@ let displayMap = (lat, long) => {
         container._leaflet_id = null;
     }
 
-    let map = L.map('map').setView([lat, long], 13);
+    let map = L.map('map', {
+        zoomControl: true,
+    });
+    map.zoomControl.setPosition('bottomright');
+    map.setView([lat, long], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
