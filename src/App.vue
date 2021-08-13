48e9217a33c99c38c5004261337dd2e48ec185e8
blade-01 <animashauntaofiq@yahoo.com>
2021-08-13 01:36:49 +0100

    Chore: Codebase changed

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <Todo />
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+@import './assets/css/themes.css';
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 15px;
+  background: var(--body-bg);
+}
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  transition: all 0.5s ease;
+}
+button {
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.container {
+  width: 90%;
+  margin: auto;
+  overflow: hidden;
+}
+@media screen and (min-width: 700px) {
+  .container {
+    width: 500px;
+  }
+}
+</style>
---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="todo-filter" @click="toggleActive">
+    <button @click="$emit('show-all')">All</button>
+    <button @click="$emit('show-active')">Active</button>
+    <button @click="$emit('show-completed')">Completed</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoFilter',
+  emits: ['show-completed', 'show-all', 'show-active'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.todo-filter {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 1rem;
+  background: var(--card-bg);
+  border-radius: 5px;
+  margin-top: 1rem;
+}
+.todo-filter button {
+  color: var(--completed-color);
+  font-weight: bold;
+}
+.todo-filter button:hover,
+.todo-filter button:active {
+  color: var(--card-color);
+}
+.todo-filter button.isActive {
+  color: var(--blue-bright);
+}
+@media screen and (min-width: 700px) {
+  .todo-filter {
+    border-radius: 0 0 5px 5px;
+    margin-top: 0;
+  }
+}
+</style>
